b21f86202628c0d5492ece9232fbc6997885e36b
Kohtaroh <kohtaroh881@gmail.com>
2024-12-15 11:28:25 +0900

    Refactored _check_events and _update_screen.

--- alien_invasion.py
+++ alien_invasion.py
@@ -19,20 +19,23 @@ class AlienInvasion:
         self.ship = Ship(self)
 
     def run_game(self):
-        """Start the main loop and mouse events."""
+        """Start the main loop for the game."""
         while True:
-            # Watch for keyboard and mouse events.
-            for even in pygame.event.get():
-                if even.type == pygame.QUIT:
-                    sys.exit()
-
-            # Redraw the screen during each pass through the loop.
-            self.screen.fill(self.settings.bg_color)
-            self.ship.blitme()
-
-            # Make the most recently drawn screen visible.
-            pygame.display.flip()
+            self._check_events()
+            self._update_screen()
             self.clock.tick(60)
+
+    def _check_events(self):
+        """Respond to keypress and mouse events."""
+        for even in pygame.event.get():
+            if even.type == pygame.QUIT:
+                sys.exit()
+
+    def _update_screen(self):
+        """Update images on the screen, and flip to the new screen."""
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+        pygame.display.flip()
 
 if __name__ == '__main__':
     # Make a game instance, and run the game.
